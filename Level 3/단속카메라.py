80a4d219d1b4328448044e4c175d8cc01cf790ad
joy961208 <joy961208@naver.com>
2021-02-04 21:01:14 +0900

    Py.code

---
+++ Level 3/단속카메라.py
@@ -0,0 +1,16 @@
+def solution(routes):
+    answer = 1
+    routes.sort()
+    a = routes[0]
+    for i in routes[1:]:
+
+        if a[0] <= i[0]:
+            a[0] = i[0]
+
+        if a[1] >= i[1]:
+            a[1] = i[1]
+
+        if a[0] > a[1]:
+            answer += 1
+            a = i
+    return answer
