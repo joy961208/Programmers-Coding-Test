71c46eb5eb0f770a9d9e28fd6f715c86b99bee68
joy961208 <joy961208@naver.com>
2021-02-05 13:25:47 +0900

    Py.code

---
+++ Level 3/멀리 뛰기.py
@@ -0,0 +1,12 @@
+def solution(n):
+    a = 1
+    b = 2
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 2
+    for i in range(n-2):
+        c = a + b
+        a = b
+        b = c
+    return c%1234567
