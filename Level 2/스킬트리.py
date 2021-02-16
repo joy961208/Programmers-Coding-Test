90dff79c7e78806bbeb3514b8d09391c1142c322
joy961208 <joy961208@naver.com>
2021-02-16 11:06:15 +0900

    Py.code

--- Level 2/스킬트리.py
+++ Level 2/스킬트리.py
@@ -2,9 +2,8 @@ def solution(skill, skill_trees):
     answer = len(skill_trees)
 
     for i in skill_trees:
-        skill_list = list(i)
         skill_tree = 0
-        for j in skill_list:
+        for j in i:
             if j in skill:
                 if skill[skill_tree] != j:
                     answer -= 1
