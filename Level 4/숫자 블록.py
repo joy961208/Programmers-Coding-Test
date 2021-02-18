baba67886ee2ee871f3027c5940dd9b4e45e40c9
joy961208 <joy961208@naver.com>
2021-02-19 00:09:25 +0900

    Py.code

--- Level 4/숫자 블록.py
+++ Level 4/숫자 블록.py
@@ -1,4 +1,5 @@
 import math
+
 def prime(a):
     for i in range(2,int(math.sqrt(a)+1)):
         if a%i == 0:
@@ -7,6 +8,7 @@ def prime(a):
             else:
                 return a//i
     return 1
+
 def solution(begin, end):
     answer = []
     for i in range(begin,end+1):
