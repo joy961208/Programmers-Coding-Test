49679c332626f6fc9db0832d85aad695afa6d99b
조현우 <76866069+joy961208@users.noreply.github.com>
2021-05-23 16:23:55 +0900

    Update 도둑질.py

--- Level 3/도둑질.py
+++ Level 3/도둑질.py
@@ -4,7 +4,6 @@ def solution(money):
     dp1[0] = money[0]
     dp1[1] = max(money[0:2])
     dp2[0] = 0
-
     for i in range(2, len(money)):
         if i != len(money) - 1:
             dp1[i] = max(dp1[i - 1], dp1[i] + dp1[i - 2])
